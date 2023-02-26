d31862eea1b7e50559c37cd37ded545af596169a
psycollapse <wawcraftsoma@hotmail.com>
2023-02-26 03:48:46 -0600

    adding App.js and index.css with profile picture ex

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+function NewLineText(props){
+  const text = props.text;
+  return <div>{text}</div>;
+}
+
+const lema = "Soy el lema! y debo salir tras presionar el botón, soy global"
+
+function MiBoton(){
+  return (
+    <button>Yo soy un boton</button>
+  );
+}
+
+
+const user ={
+  name: "Fernando Del Valle",
+  imageUrl: 'https://th.bing.com/th/id/OIP.QsObdHBpiTyWpyOgNZ_xtAHaHa?pid=ImgDet&rs=1',
+  imageSize: 90,
+};
+
+function Profile(){
+  return (
+    <>
+    <h1>{user.name}</h1>
+    <img
+      className='avatar'
+      src={user.imageUrl}
+      alt={'Photo of '+ user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize
+      }}
+      />
+    </>
+  )
+}
+
+ReactDom.render(
+  <div classname="App">
+    <Profile/>
+    <h1>Chupo Chupo!!! Unas jones deli</h1>
+    <MiBoton/>
+    <NewLineText text={lema}/>
+    <img className='avatar'/>
+  </div>,
+  document.getElementById('root')
+  )
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>Chupo Chupo!!! Unas jones deli</h1>
+      <MiBoton/>
+      <nl/>
+      <img className='avatar'/>
+      {lema}
     </div>
   );
 }
